8b8faa51bf260cbb4f3ccc17e643053cbf758aa5
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-04-06 19:18:48 -0500

    Created button submit to grab value of textarea

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,4 +6,19 @@ $('#currentDay').text(date);
 // Creates time blocks and places them into containers
 var container = $(".container");
 
-// Obtain date from input field upon button submit
+// Upon Button Submit
+var saveButtonEl = $('.saveBtn');
+var eventValue = $('.description');
+
+// Obtain date from input
+function handleFormSubmit() {
+    var text = $(this).siblings('textarea').val();
+    console.log(text);
+}
+
+saveButtonEl.on('click', handleFormSubmit);
+// Set data as text for input
+
+// Save data from input
+
+// Retrieve saved data
